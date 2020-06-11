04ee866aad0ab4c9b370cae5548f82d14d834611
King Elisha <ek.chibueze@gmail.com>
2020-06-11 12:37:39 +0100

    init firebase and other relevant packages

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as helmet from 'helmet';
+import * as firebaseApp from 'firebase/app';
+import { firebaseConfig } from './app/config';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.use(helmet());
+
+  firebaseApp.initializeApp(firebaseConfig);
+
   await app.listen(3000);
 }
 bootstrap();
