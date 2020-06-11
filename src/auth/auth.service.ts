960299d621b97f3c37a4856b15a900501e785477
King Elisha <ek.chibueze@gmail.com>
2020-06-11 14:22:32 +0100

    signin service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  ForbiddenException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import * as app from 'firebase/app';
+import 'firebase/database';
+import * as bycrypt from 'bcrypt';
+import { SigninDto } from './dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  dbRef = app.database().ref();
+
+  async signin(signinDto: SigninDto) {
+    const { email, password } = signinDto;
+
+    const userKey = email.replace(/\./g, '~').replace(/@/g, '~~');
+    const userRef = this.dbRef.child('users').child(userKey);
+
+    const userSnapshot = await userRef.once('value');
+
+    if (!userSnapshot.exists())
+      throw new NotFoundException(
+        'Your account does not exist, please sign up',
+      );
+
+    const user = userSnapshot.val();
+
+    // user.confirmed may not exist for earlier users
+    if (user.confirmed === false)
+      throw new ForbiddenException('Your account has not been verified');
+
+    const isPasswordValid = await this.validatePassword(
+      password,
+      user.password,
+    );
+
+    if (!isPasswordValid)
+      throw new UnprocessableEntityException('Incorrect Password');
+
+    return user;
+  }
+
+  private async validatePassword(
+    password: string,
+    userPassword: string,
+  ): Promise<boolean> {
+    return bycrypt.compare(password, userPassword);
+  }
+}
--- src/auth/dto/signin.dto.ts
+++ src/auth/dto/signin.dto.ts
@@ -1,4 +1,9 @@
 export class SigninDto {
-  username: string;
+  email: string;
   password: string;
+
+  constructor(email: string, password: string) {
+    this.email = email;
+    this.password = password;
+  }
 }
