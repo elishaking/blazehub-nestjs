6ae85fe2fab60cc395784ce330dca3cd1b273273
King Elisha <ek.chibueze@gmail.com>
2020-06-12 13:42:01 +0100

    signup dto

---
+++ src/auth/dto/signup.dto.ts
@@ -0,0 +1,40 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsEmail } from 'class-validator';
+
+type GenderType = 'Male' | 'Female' | 'Other';
+
+export class SignupDto {
+  @ApiProperty()
+  @IsEmail()
+  readonly email: string;
+
+  @ApiProperty()
+  @IsString()
+  readonly firstName: string;
+
+  @ApiProperty()
+  @IsString()
+  readonly lastName: string;
+
+  @ApiProperty()
+  @IsString()
+  readonly password: string;
+
+  @ApiProperty()
+  @IsString()
+  readonly gender: GenderType;
+
+  constructor(
+    email: string,
+    firstName: string,
+    lastName: string,
+    password: string,
+    gender: GenderType,
+  ) {
+    this.email = email;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.password = password;
+    this.gender = gender;
+  }
+}
