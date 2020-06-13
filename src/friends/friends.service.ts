86d7773f79abbe690f52e69a9e9e02d45eb44d16
King Elisha <ek.chibueze@gmail.com>
2020-06-13 14:32:24 +0100

    added createFriend service

--- src/friends/friends.service.ts
+++ src/friends/friends.service.ts
@@ -14,4 +14,26 @@ export class FriendsService {
 
     return friendsSnapShot.exists() ? friendsSnapShot.val() : [];
   }
+
+  async createFriend(data: any) {
+    const { userid, friendId, friend, user } = data;
+
+    // add new-friend to current-user's friends db
+    await this.dbRef
+      .child('friends')
+      .child(userid)
+      .child(friendId)
+      .set(friend);
+
+    // add current-user to new-friend's db
+    await this.dbRef
+      .child('friends')
+      .child(friendId)
+      .child(userid)
+      .set({
+        name: `${user.firstName} ${user.lastName}`,
+      });
+
+    return { [friendId]: friend };
+  }
 }
