0c482d266db36015e22bb2ac97ba6ae87aec86d1
King Elisha <ek.chibueze@gmail.com>
2020-08-08 19:57:07 +0100

    feat: added user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { EmailModule } from './email/email.module';
 import { FriendsModule } from './friends/friends.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, EmailModule, FriendsModule],
+  imports: [AuthModule, EmailModule, FriendsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
